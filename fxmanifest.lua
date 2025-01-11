2e8dd6d69c0dd03d38e50d1636a286129b15d792
dexty1 <77596597+dexty1@users.noreply.github.com>
2025-01-11 09:45:39 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Dexty'
+description 'Logit. Lähettää discord serverille tiedon palvelimella tapahtuvista asioista'
+version '1.2.0'
+
+server_script 'dextys_logs.lua'
