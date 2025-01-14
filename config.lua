4435107d7033b6be8caf4e58e540e303d45cab7e
dexty1 <77596597+dexty1@users.noreply.github.com>
2025-01-14 16:22:58 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.Webhooks = {
+    deathLogWebhook = 'https://discord.com/api/webhooks/your-webhook-url',
+    jailLogWebhook = 'https://discord.com/api/webhooks/your-webhook-url',
+    cheatLogWebhook = 'https://discord.com/api/webhooks/your-webhook-url',
+    billingLogWebhook = 'https://discord.com/api/webhooks/your-webhook-url',
+    transactionLogWebhook = 'https://discord.com/api/webhooks/your-webhook-url',
+}
